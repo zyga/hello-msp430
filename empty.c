cf9e11d0ab78eb6beb0dc276024d272636b776b4
Zygmunt Krynicki <zkrynicki@gmail.com>
2012-09-29 21:00:38 +0200

    Add empty busy-loop example
    Signed-off-by: Zygmunt Krynicki <zkrynicki@gmail.com>

---
+++ empty.c
@@ -0,0 +1,12 @@
+/**
+ * Empty program for MSP430 MCU
+ *
+ * Good candidate to check if your toolchain works as expected
+ * and that you can re-program your microcontroller board.
+ **/
+#include <msp430.h>
+
+int main(void) {
+    for (;;) {
+    }
+}
